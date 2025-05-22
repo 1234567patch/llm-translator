a885f8c0efb0d48ce7d358deea4c7694c353bf64
1234567patch <keikei00100@gmail.com>
2025-05-23 00:34:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -283,6 +283,7 @@ function updateModelList() {
         ],
         'google': [
             'gemini-2.0-flash-thinking-exp',
+            'gemini-2.5-flash-preview-05-20',
             'gemini-2.5-pro-preview-05-06',
             'gemini-2.0-flash-exp',
             'gemini-exp-1206',
